0977bcf300e6a3d394a8c93884dfe3921c807282
Mariam Nersisyan <mariamnersisyan97@gmail.com>
2022-02-12 18:07:02 -0500

    commit tree 00b467045a9c8ef05701d278553b12d91c82d24a

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solutions in this file
+// const gifts = ["teddy bear", "drone", "doll"];
+
+// function wrapGifts(gifts) {
+//   for (let i = 0; i < gifts.length; i++) {
+//     console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//     debugger;
+//   }
+
+//   return gifts;
+// }
+
+// wrapGifts(gifts);
+
+function writeCards(names, surprise) {
+    const newArray = []
+    for (let i = 0; i < names.length; i++) {
+        newArray.push(`Thank you, ${names[i]}, for the wonderful ${surprise} gift!`)
+        // debugger;
+    }
+    return newArray;
+}
+
+
+function countDown(number) {
+    let n = number
+    while (n >= 0)
+    console.log(n--);
+
+}
+countDown(10);
+
